d58b8fe44da914b2b9060cd13c7560c1acd06f2e
Odmilson <48760058+Odmilson@users.noreply.github.com>
2023-05-10 00:28:39 -0300

    Exercicio 07

---
+++ ex07/Program.cs
@@ -0,0 +1,11 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Insira seu nome:");
+        string nome = Console.ReadLine();
+
+        Console.WriteLine("Olá meu nome é: " + nome);
+        Console.ReadLine();
+    }
+}
