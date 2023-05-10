65fb11c785483c6fb9bc84655ffc00b229837578
Odmilson <48760058+Odmilson@users.noreply.github.com>
2023-05-10 00:26:06 -0300

    Exercicio 02

---
+++ ex02/Program.cs
@@ -0,0 +1,18 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Insira o valor em graus celcius:");
+        double c;
+        while(!double.TryParse(Console.ReadLine().Replace('.', ','), out c) || c <= -273.16){
+            Console.WriteLine("Valor invalido! Digite novamente:");
+        }
+
+        double f = (9 * c + 160) / 5;
+
+
+
+        Console.WriteLine("O valor convertido em Fahrenheit é:" + Convert.ToString(f));
+        Console.ReadLine();
+    }
+}
