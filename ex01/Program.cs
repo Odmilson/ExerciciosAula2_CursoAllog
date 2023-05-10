ba17ee14ba553a7957f0bb2eab41c992e3354b9f
Odmilson <48760058+Odmilson@users.noreply.github.com>
2023-05-10 00:25:37 -0300

    Exercicio 01

---
+++ ex01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Insira o valor da cotação:");
+        double cotacao;
+        while(!double.TryParse(Console.ReadLine().Replace('.', ','), out cotacao) || cotacao <= 0){
+            Console.WriteLine("Valor invalido! Digite novamente:");
+        }
+
+        Console.WriteLine("Insira o valor em dolar:");
+        double dolar;
+        while (!double.TryParse(Console.ReadLine().Replace('.', ','), out dolar) || dolar <= 0)
+        {
+            Console.WriteLine("Valor invalido! Digite novamente:");
+        }
+
+        double real = dolar * cotacao;
+
+
+
+        Console.WriteLine("O valor convertido é:" + Convert.ToString(real));
+        Console.ReadLine();
+    }
+}
