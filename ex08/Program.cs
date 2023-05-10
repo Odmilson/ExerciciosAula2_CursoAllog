f01c56e21094e6f92abb52761fbe8e3588deac0b
Odmilson <48760058+Odmilson@users.noreply.github.com>
2023-05-10 00:29:01 -0300

    Exercicio 08

---
+++ ex08/Program.cs
@@ -0,0 +1,25 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Insira seu nome:");
+        double salario;
+
+        while(!double.TryParse(Console.ReadLine(), out salario) || salario <= 0)
+        {
+            Console.WriteLine("Valor Invalido ! Insira novamente:");
+        }
+
+        if (salario <= 1849.99)
+        {
+            salario += 320;
+        }
+        else
+        {
+            salario += 180;
+        }
+
+        Console.WriteLine("O salário com reajuste é: " + salario.ToString("F2"));
+        Console.ReadLine();
+    }
+}
