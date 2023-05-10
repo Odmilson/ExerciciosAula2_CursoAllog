d599cb4c874f4290a66051eb228469563ce18bd1
Odmilson <48760058+Odmilson@users.noreply.github.com>
2023-05-10 00:27:33 -0300

    Exercicio 06

---
+++ ex06/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Insira uma frase:");
+        string a = Console.ReadLine();
+
+        char[] frase = a.ToCharArray();
+
+        for(int i = 0; i < frase.Length; i++)
+        {
+            if(frase[i] == 'A' || frase[i] == 'a')
+            {
+                frase[i] = '&';
+            }
+        }
+
+        Console.WriteLine(frase);
+        Console.ReadLine();
+    }
+}
